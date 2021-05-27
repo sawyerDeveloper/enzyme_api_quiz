5935c40e30134726fcf7c470fe8c76de147c64d8
“Rose <rose@cityhilldev.com>
2021-05-27 14:31:34 -0400

    added flex styling to DataListItem

--- src/DataListItem.js
+++ src/DataListItem.js
@@ -4,14 +4,28 @@ const DataListItem = (props) => {
 
     const styles = {
         container: {
-
+            display: 'flex',
+            //flex: 1,
+            justifyContent: 'center'
+        },
+        maybe: {
+            alignItems: 'center'
+        },
+        learnMore: {
+            alignSelf: 'flex-end',
+            marginLeft: 5
         }
     }
     return (<div style={styles.container}>
-                <h1>{props.item.selector}</h1>
-                <div>{props.item.description}</div>
-                <div><a href={props.item.link} target="_blank">Learn More </a></div>
-            </div>)
+        <div style={styles.maybe}>
+            <h1>{props.item.selector}</h1>
+            <div>{props.item.description}</div>
+        </div>
+        <div style={styles.learnMore}>
+            <a href={props.item.link} target="_blank">Learn More </a>
+        </div>
+    </div>
+    )
 }
 
 export default DataListItem
