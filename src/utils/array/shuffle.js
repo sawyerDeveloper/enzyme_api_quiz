17cf098584f75a1f74d8049a0a9430ef1c8a8e7c
Anthony Sawyer <Sawyer.developer@gmail.com>
2023-04-08 16:18:47 -0400

    better shuffle

--- src/utils/array/shuffle.js
+++ src/utils/array/shuffle.js
@@ -1,16 +1,3 @@
 export const shuffle = (array) => {
-    let newArray = structuredClone(array)
-    let currentIndex = array.length
-    let temporaryValue
-    let randomIndex
-
-    while (0 !== currentIndex) {
-        randomIndex = Math.floor(Math.random() * currentIndex)
-        currentIndex -= 1
-        temporaryValue = array[currentIndex]
-        newArray[currentIndex] = array[randomIndex]
-        newArray[randomIndex] = temporaryValue
-    }
-
-    return newArray
+    return array.sort(() => Math.random() - 0.5)
 }
